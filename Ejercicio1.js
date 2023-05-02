bc8c1d7e4a1dc4e68c43c6f488be4808cfb55fba
jesusGueGon <jesus.guerrero@bosonit.com>
2023-05-02 11:38:21 +0200

    Ejercicio1

---
+++ Ejercicio1.js
@@ -0,0 +1,15 @@
+function cogerArray() {
+
+    const arrNames = [
+        {id: 1, name: 'Pepe'},
+        {id: 2, name: 'Juan'},
+        {id: 3, name: 'Alba'},
+        {id: 4, name: 'Toby'},
+        {id: 5, name: 'Lala'}
+    ];
+
+    console.log(arrNames[2]);
+
+}
+
+cogerArray();
